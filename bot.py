a9652c31cf4c548121a45b6ec4d837ebf2269c0a
bluesky62 <92642182+bluesky62@users.noreply.github.com>
2021-10-30 23:29:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from ClointFusion import ClointFusion as Cf
+import ClointFusion as Cf
 import pytube
 from pytube import YouTube
 import os
@@ -14,47 +14,27 @@ import array
 #   percent = (100*(file_size-remaining))/file_size
 #   print(f"{percent:00.00}% downloaded")
 
-
+copy = Cf.excel_copy_range_from_sheet(excel_path="", startCol=0, startRow=0, endRow=0, endCol=0)
+print(copy)
 
 def file_path():
     home = os.path.expanduser('~')
     downlod_path = os.path.join(home, 'C:\Python39')
     return downlod_path
-copy =Cf.excel_copy_range_from_sheet(excel_path="", startCol=0, startRow=0, endRow=0, endCol=0)
-print(copy)
-#copy= [['https://www.youtube.com/watch?v=Ncm_yzScCpQ'],['https://www.youtube.com/watch?v=9JXeYfVNpEs']]
-#print(copy)
-# for i in range(len(copy)):
-#     def download(copy):
-#
-#         print('current i :', copy[i])
 
-#balance = array.array('i',(copy))
-# for i in range(0,4):
-#     print(i)
-     # browser = Cf.browser_activate(url:= "https://www.youtube.com/",
-#                      files_download_path:= 'C:\Python39
-#                      dummy_browser:= True,
-#                      open_in_background:= False,
-#                      incognito:= False,
-#                      clear_previous_instances:= False,
-#                      profile:= "Default")
-#link = Cf.gui_get_any_input_from_user("")
-#write = Cf.browser_write_h(Value:= link, User_Visible_Text_Element:= "Search")
-#enter = Cf.browser_hit_enter_h()
-#webUrl = urllib.request.urlopen('link')
-
-#drag = Cf.mouse_move(x=300,y=300)
-#click = Cf.browser_mouse_click_h(User_Visible_Text_Element:=link,element:= "1",double_click:= False,right_click:= True)
 
 def progress_func(self, chunk, bytes_remaining):
     for i in tqdm(range(100),desc="downloading...", ascii=False, ncols=75):
         time.sleep(0.01)
+
 def complete_func(self, file_path):
-    pass
+    excel = Cf.excel_set_single_cell(excel_path="", sheet_name="Sheet1", header=0, columnName="status", cellNumber=0, setText="")
+    print(excel)
+
+
 def my_proxies():
     pass
-#global copy
+## here use downlaoding function also using for loop for multiple vidoes downloade in single click
 for i in range(len(copy)):
 
     def download(copy):
@@ -62,19 +42,12 @@ for i in range(len(copy)):
         print("Your video will saved to: {}".format(file_path()))
         print("Accessing Youtube URL....")
         youtube = pytube.YouTube(copy[0], on_progress_callback=progress_func, on_complete_callback=complete_func, use_oauth=False, allow_oauth_cache=True)
-    #mp4files = youtube.filter('mp4')
-    #video = youtube.get(mp4files[-1].extension,mp4files[-1].resolution)
-
         video = youtube.streams.first()
-
-    #video.stream_to_buffer(buffer=1).filter(progressive=True, file_extension="mp4", file_size=MB)
-    #video.order_by(resolution)
         title = video.title
-        print("FEtching:{}...".format(title))
+        print("Fetching:{}...".format(title))
         video.download('C:\Python39')
         print("download is completed...")
         print("Ready to Download another video. \n\n")
 
 
     download(copy[i])
-Cf.excel_set_single_cell(excel_path="", sheet_name="Sheet1", header=0, columnName="", cellNumber=0, setText="")
